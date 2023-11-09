2276ec6b7b873493d353a1c4c2d3ed73d07c0ccd
eddDevDz95 <houedd608@gmail.com>
2023-11-09 22:29:58 +0100

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,32 @@
 import Home from "@/components/Home";
+import axios from "axios";
+
+export async function generateMetadata(paramKey: any){
+  let searchMovie = paramKey.searchParams.movie || null;
+  if (searchMovie === null) {
+    searchMovie = "avengers";
+  }
+  const movie = await axios
+  .get(`https://api.themoviedb.org/3/search/movie`, {
+        params: {
+          api_key: process.env.NEXT_PUBLIC_API_KEY,
+          query: searchMovie,
+        },
+      })
+  return {
+    title: movie?.data().title,
+    description: movie?.data().overview,
+    other: {
+      'theme-color': '#0d1117',
+      "color-scheme": "light only",
+      "twitter:image": `https://image.tmdb.org/t/p/w500/${movie?.data().poster_path}`,
+      "twitter:card": "summary_large_image",
+      "og:url": "oumat-iqraa.com",
+      "og:image": `https://image.tmdb.org/t/p/w500/${movie?.data().poster_path}`,
+      "og:type": "website",
+    }
+    }
+}
 
 export default function HomePage() {
   return <Home />;
